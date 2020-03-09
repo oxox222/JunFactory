114b3308aad909376fe7434e7a911ed93f7233ad
潘律彰 <741997717@qq.com>
2020-03-09 11:46:58 +0800

    日志打印

--- src/main/java/com/example/sale/serviceImpl/OrderServiceImpl.java
+++ src/main/java/com/example/sale/serviceImpl/OrderServiceImpl.java
@@ -4,6 +4,8 @@ import com.example.sale.dao.*;
 import com.example.sale.model.*;
 import com.example.sale.service.OrderService;
 import com.example.sale.util.UUidUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -19,6 +21,8 @@ import java.util.List;
  */
 @Service
 public class OrderServiceImpl implements OrderService {
+
+    private static Logger logger = LoggerFactory.getLogger(OrderServiceImpl.class);
 
     @Resource
     private TsalesMapper tsalesMapper;
