8e5ae53f13b49eaa187b55b04a068a10ff8250d0
潘律彰 <741997717@qq.com>
2021-01-25 17:25:51 +0800

    添加caffeine缓存

---
+++ src/main/java/com/example/sale/config/ErpDataSourceConfig.java
@@ -0,0 +1,10 @@
+package com.example.sale.config;
+
+/**
+ * @ClassName: ErpDataSourceConfig
+ * @Description: TODO
+ * @Author: PANLVZ
+ * @Date: 2020/12/16
+ */
+public class ErpDataSourceConfig {
+}
---
+++ src/main/java/com/example/sale/config/SaleDataSourceConfig.java
@@ -0,0 +1,10 @@
+package com.example.sale.config;
+
+/**
+ * @ClassName: DataSouceConfig
+ * @Description: TODO
+ * @Author: PANLVZ
+ * @Date: 2020/12/16
+ */
+public class DataSouceConfig {
+}
